20468bc36fd841a144af0964965f63f59751af10
cappeloow <cappe1996@hotmail.com>
2023-08-15 10:29:20 +0200

    react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import { io } from "socket.io-client";
+import Lobby from "./pages/Lobby";
 import LandingPage from "./pages/LandingPage";
+import { Route, Routes } from "react-router-dom";
 function App() {
   const socket = io("http://localhost:3000/");
   return (
     <>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/lobby" element={<Lobby />} />
+      </Routes>
     </>
   );
 }
