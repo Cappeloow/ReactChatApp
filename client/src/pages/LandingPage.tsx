e461e85d62be4c309c687fdff08f98917f71e9ac
cappeloow <cappe1996@hotmail.com>
2023-08-16 10:11:38 +0200

    userinput

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import "../styles/LandingPage.css";
 import { useNavigate } from "react-router-dom";
-type Props = {};
+import { useState } from "react";
+import { io } from "socket.io-client";
 
-function LandingPage({}: Props) {
+function LandingPage() {
+  const [username, setUsername] = useState("");
+  const socket = io("http://localhost:3000/", { autoConnect: false });
   const navigate = useNavigate();
+
   const handleClick = () => {
-    navigate("/lobby");
+    if (username !== "") {
+      socket.connect();
+      navigate("/lobby");
+    } else {
+      console.log("Enter a username");
+    }
   };
   return (
     <div className="mainContainer">
       <div className="loginForm">
         <h1>Login</h1>
-        <input placeholder="Användarnamn..."></input>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Användarnamn..."
+        ></input>
         <button onClick={handleClick}>Anslut</button>
       </div>
     </div>
