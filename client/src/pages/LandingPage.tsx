be6438ad05bcdabc170c8290b99ae093273e58d0
samzih <samzih1337@gmail.com>
2023-08-21 19:55:51 +0200

    simplified username conditional

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -4,12 +4,10 @@ import { useChatContext } from "../context/ChatContext";
 
 function LandingPage() {
   const { username, setUsername, connectToServer } = useChatContext();
-  // const socket = io("http://localhost:3000/", { autoConnect: false });
   const navigate = useNavigate();
 
   const handleClick = () => {
-    if (username !== "") {
-      //connect();
+    if (username) {
       connectToServer(username);
       navigate("/lobby");
     } else {
