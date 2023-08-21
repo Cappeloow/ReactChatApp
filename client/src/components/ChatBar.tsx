c00b74798f28c3bc41cc8acf08a97dad351fe5d7
samzih <samzih1337@gmail.com>
2023-08-21 17:00:48 +0200

    active users are now under the correct room

--- client/src/components/ChatBar.tsx
+++ client/src/components/ChatBar.tsx
@@ -1,30 +1,27 @@
-import React from "react";
 import "../styles/ChatBar.css";
+import { useChatContext } from "../context/ChatContext";
 
 type Props = {};
-import { useChatContext } from "../context/ChatContext";
+
 function ChatBar({}: Props) {
   const { roomList, setRoom } = useChatContext();
-  console.log(roomList);
+  console.log("roomList:", roomList);
+  
   return (
     <aside>
+      
       <h1>Chatt App</h1>
-      <p>
-        Rum:
-        {roomList.map((room, i) => (
-          <ul key={i}>
-            <li onClick={() => setRoom(room.name)}>{room.name}</li>
-          </ul>
-        ))}
-      </p>
-      <p>medlemmar</p>
-      <div>
-        {roomList.map((room, i) => (
-          <ul key={i}>
+      <h2>Rum:</h2> 
+      
+      {roomList.map((room, i) => (
+        <ul key={i}>
+          <li onClick={() => setRoom(room.name)}>{room.name}</li>
+          <ul>
             <li>{room.participants}</li>
           </ul>
-        ))}
-      </div>
+        </ul>
+      ))}
+
     </aside>
   );
 }
